3521770f0a387b584b8e3445e18c670a47c20b6d
ElectroRehab <52139014+ElectroRehab@users.noreply.github.com>
2019-06-23 16:47:44 -0400

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_JonKing.py
@@ -0,0 +1,13 @@
+# This assignment is teaching us how to program a user friendly percentage of profit margin form.
+# 18JUN19
+# CTI-110 P2T1 - Sales Prediction
+# Jon King
+#
+total_sales = float(input('Enter the projected sales: '))
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+#Diplay the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+      
+      
